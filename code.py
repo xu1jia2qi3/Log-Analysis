ba17bdd9f040d2424e1664c2e41c06d5270d64dd
xu1jia2qi3 <jiaqy@live.com>
2018-10-13 15:34:19 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python3
 import psycopg2
+
+try:
+    db = psycopg2.connect("dbname=news")
+except:
+    print("Unable to connect to the database")
 
 
 def most_popular_three_articles():
-    db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
     cursor.execute("select articles.title, count(log.path) as views "
                    "from articles join log "
@@ -16,7 +21,6 @@ def most_popular_three_articles():
 
 
 def most_popular_authors():
-    db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
     cursor.execute("select authors.name, count(*) as views "
                    "from articles, authors, log "
@@ -30,21 +34,24 @@ def most_popular_authors():
 
 
 def percentageerror():
-    db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
     cursor.execute("create view error_num as "
                    "select cast(time as date),count(*) as error from log "
                    "where status = '404 NOT FOUND' "
                    "group by cast(time as date); "
+
                    "create view total_num as "
                    "select cast(time as date),count(*) as total from log "
                    "group by cast(time as date); "
+
+                   "create view result as "
                    "select error_num.time, "
                    "round(error_num.error * 100.0/total_num.total, 1) "
                    "as percentage "
                    "from error_num, total_num "
-                   "where error_num.time = total_num.time "
-                   "order by percentage desc; "
+                   "where error_num.time = total_num.time; "
+
+                   "select * from result where percentage >1;"
                    )
     result = cursor.fetchall()
     print(result)
@@ -54,3 +61,7 @@ def percentageerror():
 most_popular_authors()
 most_popular_three_articles()
 percentageerror()
+
+
+
+
